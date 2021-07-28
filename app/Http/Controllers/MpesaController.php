8bc4c1fb254e07c3b57685ed01361f1d5ec0fae1
Derrick Mwema <derrickmbugua50@gmail.com>
2021-07-28 09:42:10 +0300

    feat: Customer To Business Register URL

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\MpesaTransaction;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class MpesaController extends Controller
 {
-      /**
+    /**
      * Lipa na M-PESA password
      * */
     public function lipaNaMpesaPassword()
@@ -16,8 +19,8 @@ class MpesaController extends Controller
         $lipa_time = Carbon::rawParse('now')->format('YmdHms');
         $passkey = getenv('MPESA_PASSKEY');
         $BusinessShortCode = 174379;
-        $timestamp =$lipa_time;
-        $lipa_na_mpesa_password = base64_encode($BusinessShortCode.$passkey.$timestamp);
+        $timestamp = $lipa_time;
+        $lipa_na_mpesa_password = base64_encode($BusinessShortCode . $passkey . $timestamp);
         return $lipa_na_mpesa_password;
     }
     /**
@@ -28,7 +31,7 @@ class MpesaController extends Controller
         $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generateAccessToken()));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $this->generateAccessToken()));
         $curl_post_data = [
             //Fill in the request parameters with valid values
             'BusinessShortCode' => 174379,
@@ -81,4 +84,63 @@ class MpesaController extends Controller
         return $response;
     }
 
+    /**
+     *  M-pesa Validation Method
+     * Safaricom will only call your validation if you have requested by writing an official letter to them
+     */
+    public function mpesaValidation(Request $request)
+    {
+        Log::info("Validation endpoint hit");
+        Log::info($request->all());
+    }
+    /**
+     * M-pesa Transaction confirmation method, we save the transaction in our databases
+     */
+    public function mpesaConfirmation(Request $request)
+    {
+        Log::info("Confirmation endpoint hit");
+        Log::info($request->all());
+    }
+
+    /**
+     * M-pesa Register Validation and Confirmation method
+     */
+    public function mpesaRegisterUrls()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl');
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer ' . $this->generateAccessToken()));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array(
+            'ShortCode' => "600983",
+            'ResponseType' => 'Completed',
+            'ConfirmationURL' => "https://186addcf06ed.ngrok.io/api/v1/confirmation",
+            'ValidationURL' => "https://186addcf06ed.ngrok.io/api/v1/validation"
+        )));
+        $curl_response = curl_exec($curl);
+        return $curl_response;
+    }
+
+    /**
+     * M-pesa Register URLS
+     */
+
+    public function registerUrls()
+    {
+
+        $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer ' . $this->generateAccessToken()));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array(
+            "ShortCode" => "600983",
+            "ResponseType" => "Completed",
+            "ConfirmationURL" => "https://186addcf06ed.ngrok.io/api/v1/confirmation",
+            "ValidationURL" => "https://186addcf06ed.ngrok.io/api/v1/validation",
+        )));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response     = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('v1/access-token', [MpesaController::class, "generateAccessToken"]);
 Route::post('v1/token', [MpesaController::class, "token"]);
 Route::post('v1/stkpush', [MpesaController::class, "stkPush"]);
+Route::post('v1/validation', [MpesaController::class, "mpesaValidation"]);
+Route::post('v1/confirmation', [MpesaController::class, "mpesaConfirmation"]);
+Route::post('v1/register/url', [MpesaController::class, "mpesaRegisterUrls"]);
+Route::post('v1/register-url', [MpesaController::class, "registerUrls"]);
